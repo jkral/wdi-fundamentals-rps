de846b331b76d0843c345cf3516de76f7da0a00d
Jeff Kral <jeffkral@Jeffs-MacBook-Pro.local>
2015-09-24 17:25:01 -0400

    simplified winner

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -25,7 +25,7 @@ function getPlayerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `getInput()`.
-    if (move != null) {
+    if (move !== null) {
         return move
     }  else {
         return getInput()
@@ -37,7 +37,7 @@ function getComputerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
-     if (move != null) {
+     if (move !== null) {
         return move
     }  else {
         return randomPlay()
@@ -51,6 +51,17 @@ function getWinner(playerMove,computerMove) {
     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
     // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
     /* YOUR CODE HERE */
+    if (playerMove == computerMove) {
+        winner = 'tie'
+    } else if (playerMove == 'rock' && computerMove == 'scissors'{
+        winner = 'player'
+    } else if (playerMove == 'scissors' && computerMove == 'paper') {
+        winner = 'player'
+    } else if (playerMove == 'paper' && computerMove == 'rock') {
+        winner = 'player'
+    } else {
+        winner = 'computer'
+    }    
     return winner;
 }
 
@@ -60,6 +71,21 @@ function playToFive() {
     var computerWins = 0;
     // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
     /* YOUR CODE HERE */
+    while(playerWins < 5 && computerWins < 5) {
+        getPlayerMove(getInput(playerMove))
+        return playerMove;
+
+        getComputerMove(randomPlay(computerMove))
+        return computerMove;
+        
+        getWinner(playerMove,computerMove)
+
+
+
+
+
+
+    }
     return [playerWins, computerWins];
 }
 
